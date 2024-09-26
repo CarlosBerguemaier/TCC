7b9c84b682c22a1168e052fa215105ebccc7d6b7
Carlos <carloseduardogindri@gmail.com>
2024-09-26 15:50:44 -0300

    atualizando index.html

--- js.js
+++ js.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
 
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+//var collapsibleElem = document.querySelector('.collapsible');
+//var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
 
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
