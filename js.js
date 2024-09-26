73cbecbba02f4aa5225dff27b0336512719be68c
Carlos <carloseduardogindri@gmail.com>
2024-09-26 14:43:25 -0300

    alterado menu superior para menu lateral

--- js.js
+++ js.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems, options);
   });
 
-  // Or with jQuery
 
   $(document).ready(function(){
     $('.sidenav').sidenav();
