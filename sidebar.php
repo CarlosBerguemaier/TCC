fdeae44be586fe3496175849331736f403e5c615
Carlos <carloseduardogindri@gmail.com>
2024-10-09 21:14:56 -0300

    Alterado organização da tela cadastros

--- sidebar.php
+++ sidebar.php
@@ -14,12 +14,15 @@
     <li><a href="../view/telaBusca.php" class="removerLink"><i class="material-icons">search</i>Buscar</a></li>
     <li><a href="../view/telaRelatorio.php" class="removerLink"><i class="material-icons">library_books</i>Relatório</a></li>
   </ul>
-  <nav class="black"><a href="#" data-target="slide-out" class="sidenav-trigger removerLink"><i class="material-icons">menu</i></a>
+  <nav class="black"><a href="#" data-target="slide-out" class="sidenav-trigger removerLink">
+    <i class="material-icons">menu</i></a>
+    <span href="#" class=" center texto-principal">Gerenciamento da Oficina</span>
   </nav>
   </div>
 </body>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <script src="js.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
--- view/telaCadastro.php
+++ view/telaCadastro.php
@@ -15,89 +15,66 @@
 <?php include_once '../sidebar.php';?>
 
 <div class="principal">
+<div>
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="servico-tab" data-toggle="tab" href="#servico" role="tab" aria-controls="servico" aria-selected="true">Serviços</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="cliente-tab" data-toggle="tab" href="#cliente" role="tab" aria-controls="cliente" aria-selected="false">Clientes</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="carro-tab" data-toggle="tab" href="#carro" role="tab" aria-controls="carro" aria-selected="false">Carros</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="funcionario-tab" data-toggle="tab" href="#funcionario" role="tab" aria-controls="funcionario" aria-selected="false">Funcionários</a>
+  </li>
+</ul>
 
-<div class="accordion" id="accordionExample">
-  <div class="card">
-    <div class="card-header" id="headingOne">
-      <h5 class="mb-0">
-        <button class="btn collapsed bg-light text-dark" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Serviços
-        </button>
-      </h5>
-    </div>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active bg-white border" id="servico" role="tabpanel" aria-labelledby="servico-tab"> 
+       <div class="card-body">
+      <form action="">
+        Placa do Carro: <input type="text">
+        CPF do Cliente: <input type="text">
+        CPF do Funcionário: <input type="text">
+        Descrição: <input type="text">
+        Valor: <input type="text">
+        Quilometragem Inicial: <input type="text">
+        Quilometragem Final: <input type="text">
+    </form>
+</div></div>
 
-    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
-      <div class="card-body">
-      <form action="">
-        Carro: <input type="text">
-        Cliente: <input type="text">
-        Telefone: <input type="text">
+  <div class="tab-pane fade bg-white border" id="cliente" role="tabpanel" aria-labelledby="cliente-tab">
+  <form action="">
+        Nome: <input type="text">
         CPF: <input type="text">
-    </form>
-</div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingTwo">
-      <h5 class="mb-0">
-        <button class="btn collapsed bg-light text-dark" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-         Clientes
-        </button>
-      </h5>
-    </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div class="card-body">
-      <form action="">
-        Carro: <input type="text">
-        Cliente: <input type="text">
         Telefone: <input type="text">
-        CPF: <input type="text">
     </form>
-</div>
-    </div>
   </div>
-  <div class="card">
-    <div class="card-header" id="headingThree">
-      <h5 class="mb-0">
-        <button class="btn collapsed bg-light text-dark" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Carros
-        </button>
-      </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-      <div class="card-body">
-      <form action="">
-        Carro: <input type="text">
-        Cliente: <input type="text">
+  <div class="tab-pane fade bg-white border" id="carro" role="tabpanel" aria-labelledby="carro-tab">
+  <form action="">
+        Placa: <input type="text">
+        Marca: <input type="text">
+        Modelo: <input type="text">
+        Ano: <input type="text">
+    </form>
+  </div>
+    <div class="tab-pane fade bg-white border" id="funcionario" role="tabpanel" aria-labelledby="funcionario-tab">
+    <form action="">
+        Nome: <input type="text">
+        CPF: <input type="text">
         Telefone: <input type="text">
-        CPF: <input type="text">
     </form>
-</div>
     </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingFour">
-      <h5 class="mb-0">
-        <button class="btn collapsed bg-light text-dark" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseThree">
-          Funcionários
-        </button>
-      </h5>
-    </div>
-    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
-      <div class="card-body">
-      <form action="">
-        Carro: <input type="text">
-        Cliente: <input type="text">
-        Telefone: <input type="text">
-        CPF: <input type="text">
-    </form>
- </div>
-    </div>
-  </div>
 </div>
 
-</div>
 
+
+
+
+</div>
+</div>
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
