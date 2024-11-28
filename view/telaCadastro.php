322b27eed09cd720a90e9f2b2b2c01133a598493
Carlos <carloseduardogindri@gmail.com>
2024-11-28 18:03:02 -0300

    pequenas correções

--- view/telaCadastro.php
+++ view/telaCadastro.php
@@ -13,7 +13,6 @@
 
 </head>
 <body>
-
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #6AA6F9;">
 
 
@@ -76,7 +75,11 @@
        <div class="card-body">
        <form action="../control/OrdemServicoControle.php" method="post">
         Placa do Carro: <input type="text" name="placa">
-        CPF do Cliente: <input type="text" name="cpf_cliente">
+        CPF do Cliente:<button type="button" class="btn btn-secondary" data-container="body" data-toggle="popover" data-placement="right" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+        <i class="material-icons">search</i>
+</button>
+
+<input type="text" name="cpf_cliente"> 
         CPF do Funcionário: <input type="text" name="cpf_funcionario">
         Descrição: <input type="text" name="descricao">
         Valor: <input type="text" name="valor">
@@ -122,11 +125,13 @@
 
 </div>
 </div>
-</body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <script src="../js.js"></script>
 <script src="../cadastros.js"></script>
+</body>
+
 </html>
