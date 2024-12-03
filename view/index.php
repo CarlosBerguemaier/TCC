d2f16e75fe36dcf4cfceb22b2291301753f1247b
Carlos Berguemaier <carloseduardogindri@gmail.com>
2024-12-03 19:27:44 -0300

    Removendo arquivos

--- view/index.php
+++ view/index.php
@@ -6,10 +6,9 @@
     <title>Página Principal</title>
  
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 <link rel="stylesheet" href="../estilo.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #6AA6F9;">
@@ -25,20 +24,17 @@
         <a class="nav-link navbar-link" href="../view/telaCadastro.php">Cadastrar</a>
       </li>
 
-      <li class="nav-item">
-        <a class="nav-link navbar-link dropdown-toggle select" href="#" id="dropdownMenuButtonClientes" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Buscar/Editar</a>
-      
-      <div class="dropdown">
-          
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButtonClientes">
-            <a class="dropdown-item" href="../view/telaBuscaOrdemServico.php">Ordem de Serviço</a>
-            <a class="dropdown-item" href="../view/telaBuscaClientes.php">Clientes</a>
-            <a class="dropdown-item" href="../view/telaBuscaFuncionarios.php">Funcionários</a>
-            <a class="dropdown-item" href="../view/telaBuscaCarros.php">Carros</a>
-          </div>
-            </div>
-            </li>
-
+      <li class="nav-item dropdown">
+          <a class="nav-link navbar-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Buscar/Editar
+          </a>
+          <ul class="dropdown-menu">
+         <li><a class="dropdown-item" href="../view/telaBuscaOrdemServico.php">Ordem de Serviço</a></li>
+         <li><a class="dropdown-item" href="../view/telaBuscaClientes.php">Clientes</a></li>
+         <li> <a class="dropdown-item" href="../view/telaBuscaFuncionarios.php">Funcionários</a></li>
+         <li><a class="dropdown-item" href="../view/telaBuscaCarros.php">Carros</a></li>
+          </ul>
+        </li>
     </ul>
   </div>
 </nav>
@@ -58,11 +54,8 @@
     } 
   ?>
 <div class="principal"></div>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+   
 </body>
 
 </html>
